49733703d5a8d1b17ea4ea21b1b56aa07f4eb1f6
Domenico <domeniconicoli@gmail.com>
2020-05-24 11:40:27 +0200

    Initial Commit

---
+++ SwiftUIFirstSteps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirstSteps
+//
+//  Created by Domo on 24/05/2020.
+//  Copyright © 2020 Nicoli Domenico. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
